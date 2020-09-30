0826efaf960acc4a5a518ae71c43f5a32e3f51cf
atulbharti11 <52249440+atulbharti11@users.noreply.github.com>
2020-09-29 21:06:13 -0500

    Create ReturnLengthOfLastWord.java

---
+++ ReturnLengthOfLastWord.java
@@ -0,0 +1,25 @@
+/*
+Given a string a consists of upper/lowercase alphabet characters and empty space characters '',
+return the length of the last word in the string.
+If the last word doesn't exist, return 0.
+/*
+
+
+class Solutionnn {
+
+    public static int countLength(String s){
+        if(s.substring(s.length()-1).equals(" ") ) return 0;
+        if(s.length() == 0 || s.isEmpty()){
+            return 0;
+        }
+        String[] arr = s.split(" ");
+        int strLen = arr.length;
+        int lastWordLength = arr[strLen-1].length();
+        return lastWordLength;
+    }
+
+    public static void main(String[] args) {
+        String s = "Interview test for xxx company";
+        System.out.println(countLength(s));
+    }
+}
