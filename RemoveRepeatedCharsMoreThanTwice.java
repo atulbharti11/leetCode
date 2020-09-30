f554e2f52ca089a60a196360a4d54da7f7b21131
atulbharti11 <52249440+atulbharti11@users.noreply.github.com>
2020-09-30 17:21:41 -0500

    Create RemoveRepeatedCharsMoreThanTwice.java

---
+++ RemoveRepeatedCharsMoreThanTwice.java
@@ -0,0 +1,28 @@
+/*
+Remove repeated characters that appear more than twice in a string
+
+11122233 -> 112233
+1234445566666 -> 123445566
+*/
+
+class Solution {
+    public String solution(String S) {
+        // write your code in Java SE 8
+        int count = 1;
+
+        StringBuilder stb = new StringBuilder();
+        stb.append(S.charAt(0));
+
+        for(int i=1;i<S.length();i++) {
+            char c = S.charAt(i);
+            if(c == S.charAt(i-1))
+                count++;
+            else {
+                count = 1;
+            }
+            if(count < 3)
+                stb.append(c);
+        }
+        return stb.toString();
+    }
+}
