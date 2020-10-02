57bbc6c8360952bf106b8e9dd5bde2149c4ff2ea
atulbharti11 <52249440+atulbharti11@users.noreply.github.com>
2020-10-02 00:05:13 -0500

    Update smallestNumberOtherThanList.java

--- smallestNumberOtherThanList.java
+++ smallestNumberOtherThanList.java
@@ -15,6 +15,7 @@ Write an efficient algorithm for the following assumptions:
         each element of array A is an integer within the range [−1,000,000..1,000,000].
 */
 
+
 import java.util.HashMap;
 class Solution {
     public int solution(int[] A) {
