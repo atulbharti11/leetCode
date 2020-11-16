3bd459cad559b72f4be43a0d6d7b5b15dd03bb0a
atulbharti11 <52249440+atulbharti11@users.noreply.github.com>
2020-11-16 08:21:23 -0600

    Update Map1-mapBully.java

--- Map1-mapBully.java
+++ Map1-mapBully.java
@@ -1,4 +1,3 @@
-
 /* Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
 
 mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
