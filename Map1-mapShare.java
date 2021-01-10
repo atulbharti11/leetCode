e1206cd735c9fe357958b5aa37faddf2c1a2e5e2
atulbharti11 <52249440+atulbharti11@users.noreply.github.com>
2021-01-10 13:15:26 -0600

    Update Map1-mapShare.java

--- Map1-mapShare.java
+++ Map1-mapShare.java
@@ -1,5 +1,4 @@
 /*
-
 Program to modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
 
 mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
